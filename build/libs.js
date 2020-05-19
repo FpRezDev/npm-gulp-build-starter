4f679d2df29e6f76f521f1783232a274ad44077f
FpRezDev <fprez.developer@outlook.com>
2020-05-18 18:25:07 -0600

    Implement libs build tasks

--- build/libs.js
+++ build/libs.js
@@ -1,14 +1,25 @@
-const { series } = require("gulp");
+const { src, dest } = require("gulp");
 const config = require("../config/libs.json");
+const del = require("del");
+const merge = require("merge-stream");
 
-function buildLibs(cb) {
+function buildLibs() {
   console.log("Building Libs");
-  return cb();
+  var tasks = config.map(
+    function(lib)
+    {
+      console.log(`Copying: ${lib.src}`);
+      console.log(`To: ${lib.dest}`);
+      return src(lib.src)
+                .pipe(dest(lib.dest));
+    }
+  );
+  return merge(tasks);
 }
 
-function cleanLibs(cb) {
-  console.log("Cleaning Libs");
-  return cb();
+function cleanLibs() {
+  console.log("Cleaning Libs: assets/libs/");
+  return del("assets/libs/");
 }
 
 function logLibsConfig(cb) {
